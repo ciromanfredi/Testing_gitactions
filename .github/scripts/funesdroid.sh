af07b2dee5dce5c9eb09b55b68710b4678738218
ciro676 <ciromanfredi96@gmail.com>
2020-03-09 17:23:59 +0100

    From android studio

--- .github/scripts/funesdroid.sh
+++ .github/scripts/funesdroid.sh
@@ -7,6 +7,24 @@
 # Extracted from https://github.com/facebook/react-native/blob/master/scripts/android-setup.sh
 
 echo "Eseguo script funesDroid..."
+pwd
+ls
+cd app
+ls
+cd build
+ls
+cd outputs
+ls
+cd apk
+ls
+cd debug
+ls
+cd ../../../../
+ls
+cd FunesDroid-master
+ls
+cd InputAPKs
+ls
 mv app/build/outputs/apk/debug/app-debug.apk app/FunesDroid-master/InputAPKs/app-debug.apk
 cd app/FunesDroid-master/
 python launchexp.py
