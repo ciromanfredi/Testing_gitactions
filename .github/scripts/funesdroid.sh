3853a9deafbe40e933cb852e53e08bfae6b84836
ciro676 <ciromanfredi96@gmail.com>
2020-03-09 14:12:34 +0100

    From android studio

---
+++ .github/scripts/funesdroid.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#
+# Extracted from https://github.com/facebook/react-native/blob/master/scripts/android-setup.sh
+
+echo "Eseguo script funesDroid..."
+
+ls
+pwd
+cd app/
+ls
+pwd
+cd FunesDroid-master/
+ls
+pwd
+python launchexp.py
