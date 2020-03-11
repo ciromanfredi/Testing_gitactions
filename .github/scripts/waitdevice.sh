d8bf1e5a895f87752a5b17a055eaeb7fffb49a94
ciro676 <ciromanfredi96@gmail.com>
2020-03-11 19:53:40 +0100

    commit

--- .github/scripts/waitdevice.sh
+++ .github/scripts/waitdevice.sh
@@ -2,4 +2,9 @@ echo "Start wait device"
 while ! (adb shell getprop sys.boot_completed)
 do
   echo "Non si è avviato..."
+  sleep 3
 done
+echo "Disabling animations."
+adb shell settings put global window_animation_scale 0.0
+adb shell settings put global transition_animation_scale 0.0
+adb shell settings put global animator_duration_scale 0.0
